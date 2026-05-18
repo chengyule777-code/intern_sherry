7205aa0970fbf85da81b6817a4febbf9a0354222
lf@intern <lf.yeung@rehab-robotics.com.hk>
2026-05-18 14:37:11 +0800

    feat: add lf-toolbox & sherry aliases

---
+++ my_aliases.sh
@@ -0,0 +1,18 @@
+#---Sherry's git alias---
+alias gs='git status'
+alias ga='git add .'
+alias gc='git commit -m'
+alias gp='git push origin main'
+alias gl='git log --oneline --graph'
+
+#---virtual environment alias---
+alias venv='source venv/bin/activate'
+alias csh='cd ~/git/intern_sherry'
+
+#---lf-toolbox alias---
+alias lfh='lf --help' #quickly check help
+alias zip.pdf='lf pdf' 
+alias clock='lf clock'
+alias plot.csv='lf plot'
+
+
